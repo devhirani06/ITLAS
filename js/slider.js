948b38b273bdec2df132c8f461d07177681b37c6
Pratham Parmar <prathamparmar.dev@gmail.com>
2024-10-20 13:21:11 +0530

    fixed 2

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,16 @@
 // Hero Carousel
 const slider1 = document.querySelector('#glide_1');
 if (slider1) {
+  // All Glide-related code can be removed or commented out
+  /*
   new Glide(slider1, {
     type: 'carousel',
     startAt: 0,
-    // autoplay: 3000,
-    gap: 0,
-    hoverpause: true,
     perView: 1,
-    animationDuration: 800,
-    animationTimingFunc: 'linear',
+    autoplay: false,
+    hoverpause: true,
+    bound: true,
+    animationDuration: 0 // Set to 0 to disable animation
   }).mount();
+  */
 }
